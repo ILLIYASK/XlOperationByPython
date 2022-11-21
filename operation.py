5781ec2e137b2d30858388eef870990adcb7f2ed
ILLIYASK <Illiyask26061999@gmail.com>
2022-11-21 21:43:46 +0530

    barchart created

--- operation.py
+++ operation.py
@@ -1,5 +1,5 @@
 import openpyxl as xl
-
+from openpyxl.chart import BarChart,Reference
 
 def proces_workbook(filename):
    wb=xl.load_workbook(filename)
@@ -9,6 +9,11 @@ def proces_workbook(filename):
        corrected_price=cell.value*.9
        corrected_price_cell=sheet.cell(row,4)
        corrected_price_cell.value=corrected_price
+   values=Reference(sheet,min_row=2,max_row=sheet.max_row,
+                    min_col=4,max_col=4)
+   chart=BarChart()
+   chart.add_data(values)
+   sheet.add_chart(chart,'e2')
 
    wb.save(filename)
 
